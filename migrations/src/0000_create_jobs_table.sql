0b1c916a572597b67ade0515451fb5266708fc4a
Rui Alves Personal <alvesrui951@gmail.com>
2024-04-06 17:07:31 +0100

    Added first migration example

---
+++ migrations/src/0000_create_jobs_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS "jobs" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"full_name" varchar(256),
+	"created_at" timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	"updated_at" timestamp NOT NULL
+);
+--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "name_idx" ON "jobs" ("full_name");
